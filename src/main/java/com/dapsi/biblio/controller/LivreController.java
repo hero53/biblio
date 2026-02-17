9f00e658f682ab4ae3d8fa20d99ca0e60158b491
kiny <45401008+hero53@users.noreply.github.com>
2026-02-17 15:03:34 +0000

    crud de base

--- src/main/java/com/dapsi/biblio/controller/LivreController.java
+++ src/main/java/com/dapsi/biblio/controller/LivreController.java
@@ -4,10 +4,10 @@ package com.dapsi.biblio.controller;
 import com.dapsi.biblio.model.Livre;
 import com.dapsi.biblio.repository.LivreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 
 @RestController
@@ -17,7 +17,39 @@ public class LivreController {
     private LivreRepository livreRepository;
 
     @PostMapping("/addLivre")
-    public ResponseEntity<Livre> addLivre(@RequestBody Livre livre) {
+    public ResponseEntity<Livre> createLivre(@RequestBody Livre livre) {
         Livre savedLivre = livreRepository.save(livre);
         return ResponseEntity.ok(savedLivre);
     }
+
+    @PostMapping("/addListLivre")
+    public ResponseEntity<List<Livre>> addListLivre(@RequestBody List<Livre> livreList) {
+        livreRepository.saveAll(livreList);
+        return ResponseEntity.ok(livreList);
+    }
+
+    @GetMapping("/fetchLivre")
+    public ResponseEntity<List<Livre>>fetchLivre() {
+        List<Livre> lives = livreRepository.findAll();
+        return ResponseEntity.ok(lives);
+    }
+
+    @PutMapping("updateLivre/{id}")
+    public ResponseEntity<Livre> updateLivre(@PathVariable Long id, @RequestBody Livre livre) {
+        Livre savedLivre = livreRepository.save(livre);
+        return ResponseEntity.ok(savedLivre);
+    }
+
+    @DeleteMapping("/deleteLivre/{id}")
+    public ResponseEntity<Livre> deleteLivre(@PathVariable Long id) {
+        Livre isExiste = livreRepository.findById(id).orElse(null);
+        if (isExiste != null) {
+            livreRepository.delete(isExiste);
+        }else  {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(isExiste);
+    }
+
+
+}
